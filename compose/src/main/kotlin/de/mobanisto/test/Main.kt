6d77629596c729e55ff34436ab72ce940663a10a
Sebastian Kürten <sebastian@topobyte.de>
2022-02-02 09:52:30 +0100

    Add Compose window with a few widgets

--- compose/src/main/kotlin/de/mobanisto/test/Main.kt
+++ compose/src/main/kotlin/de/mobanisto/test/Main.kt
@@ -1,5 +1,17 @@
 package de.mobanisto.test
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Text
+import androidx.compose.material.TextButton
+import androidx.compose.material.TextField
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.singleWindowApplication
+
 fun main() {
     Main().run()
 }
@@ -11,5 +23,19 @@ class Main {
 
         val native = NativeClass()
         println("pid: ${native.processId}")
+
+        singleWindowApplication(title = "Test") {
+            val (value, setValue) = remember { mutableStateOf("input") }
+            Column(
+                verticalArrangement = Arrangement.spacedBy(8.dp),
+                modifier = Modifier.padding(16.dp)
+            ) {
+                Text("Test")
+                TextField(value, setValue)
+                TextButton({ setValue("") }) {
+                    Text("Clear")
+                }
+            }
+        }
     }
 }
